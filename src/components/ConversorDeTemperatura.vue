0775cb745f281ac25f8cc4d5737cd23f068a0d3c
Agus Borgo <agusborgo5@gmail.com>
2025-10-30 20:33:37 -0300

    Conversor de temperatura funcionando

--- src/components/ConversorDeTemperatura.vue
+++ src/components/ConversorDeTemperatura.vue
@@ -7,39 +7,62 @@
         <input type="number"
         id="celsius"
         class="form-control"
-        v.model.number="celsius"
+        v-model.number="celsius"
         placeholder="Ingresar temperatura">
     </div>
     <br>
     <div class="class mt-4">
-        <p>Temperatura en Fahrenheit</p>
+        <p>Temperatura en Fahrenheit: 
+            <span v-if="mostrarResultado" :style="{color : colorTexto}">{{fahrenheit.toFixed(2) }}</span>
+        </p>
+        <br>
+        <p>Temperatura en Kelvin: 
+            <span v-if="mostrarResultado" :style="{color : colorTexto}">{{kelvin.toFixed(2) }}</span>
+        </p>
+
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MiComponente',
+  name: 'ConversorDeTemperatura',
   props: {
   },
   data() {
     return {
       title: 'Conversor de temperatura',
-      // Variables reactivas
+      celsius: null,
     };
   },
   computed: {
     // Propiedades computadas
+    fahrenheit(){
+        return (this.celsius * 1.8 + 32)
+    },
+    kelvin(){
+        return this.celsius + 273.15
+    },
+    colorTexto(){
+        if(this.celsius <= 0){
+            return "blue"
+        } else if (this.celsius > 0 && this.celsius < 15){
+            return "magenta"
+        } else {
+            return "red"
+        }
+    },
+    mostrarResultado() {
+        return typeof this.celsius === 'number' && !isNaN(this.celsius);
+    }
+
   },
   methods: {
     // Funciones y lógica
-    saludar() {
-      console.log('¡Hola desde Vue!');
-    }
   },
   mounted() {
     // Código que se ejecuta al montar el componente
-    this.saludar();
+    
   }
 };
 </script>
